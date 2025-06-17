497b60b6c3dcb929a35bfc3bb436cafa30e66391
Chilet-Okeke <okekechiletaram@gmail.com>
2025-06-17 15:54:22 +0100

    callback again

--- src/functions/callbackFunc.js
+++ src/functions/callbackFunc.js
@@ -1,15 +1,32 @@
 // // create a callback function to add surname
 
-function first() {
-  return "Chilet";
+// function first(call1) {
+//   return "Chilet";
+// }
+
+// function second(call2) {
+//   return `Okeke`;
+// }
+
+// function main(call1, call2) {
+//   console.log(`My name is ${call1()} ${call2()}`);
+// }
+
+// main(first, second);
+
+
+function first(call1) {
+  return `Hello ${call1}`;
 }
 
 function second() {
-  return "Okeke";
+  return `Okeke`;
 }
 
 function main(call1, call2) {
-  console.log(`My name is ${call1()} ${call2()}`);
+  console.log(`${first(call1)}, My name is ${call2()}`);
 }
 
-main(first, second);
+main('Chilet', second);
+
+
